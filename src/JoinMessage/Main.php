16e42d6c29e90c19ca3ca9961650af97796a7fab
NameNoOne100 <morganewing1@icloud.com>
2016-01-09 20:07:08 +1100

    Create Main.php

---
+++ src/JoinMessage/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+  namespace JoinMessage;
+
+  use pocketmine\event\player\PlayerJoinEvent;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+
+  class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+
+      if(!(file_exists("JoinMessage/config.txt"))) {
+
+        @mkdir("JoinMessage/", 0777, true);
+        touch("JoinMessage/config.txt");
+
+      }
+
+    }
+
+    public function onJoin(PlayerJoinEvent $ev) {
+
+      $msg = file_get_contents("JoinMessage/config.txt");
+      $player = $ev->getPlayer();
+      $player->sendMessage($msg);
+
+   }
+
+  }
+
+?>
